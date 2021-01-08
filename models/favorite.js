7814c0b5e8acd1fa8e11500eb84ea2d23fb26df3
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-01-08 17:28:29 +0100

    added favourite model and migration

---
+++ models/favorite.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class favorite extends Model {
+        static associate(models) {
+            favorite.belongsTo(models.user);
+            favorite.belongsTo(models.post);
+        }
+    }
+    favorite.init({
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "users",
+                key: "id"
+            }
+        },
+        postId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "posts",
+                key: "id"
+            }
+        }
+    }, {
+        sequelize,
+        modelName: 'favorite',
+    });
+    return favorite;
+};
