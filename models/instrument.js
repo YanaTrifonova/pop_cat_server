dd8d53b773761b9d9ec44a7e13570450986f8065
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-01-15 16:34:46 +0100

    fixed typo in instrument model

--- models/instrument.js
+++ models/instrument.js
@@ -5,7 +5,7 @@ const {
 module.exports = (sequelize, DataTypes) => {
     class instrument extends Model {
         static associate(models) {
-            instrument.belongsTo(models.post);
+            instrument.hasMany(models.post);
 
             instrument.hasOne(models.instrumentsNote);
         }
