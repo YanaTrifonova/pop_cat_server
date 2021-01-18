0507bc82e1a278171ebd6a9037c57e14126878bc
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-01-18 14:41:44 +0100

    updated discover page

--- routers/discover.js
+++ routers/discover.js
@@ -2,6 +2,7 @@ const Router = require("express");
 const router = new Router();
 const Posts = require("../models").post;
 const Cats = require("../models").cat;
+const Users = require("../models").user;
 
 router.get('/songs', async (req, res) => {
     try {
@@ -9,6 +10,8 @@ router.get('/songs', async (req, res) => {
         if (!posts) return res.status(404).send("posts not found");
 
         const catUrls = await Cats.findAll({attributes: ["id", "url", "name"]});
+
+        const users = await Users.findAll({attributes : ["id", "name"]});
 
         let newPosts = [];
         let elem;
@@ -18,6 +21,9 @@ router.get('/songs', async (req, res) => {
             let catUrl = cat.dataValues.url;
             let catName = cat.dataValues.name;
 
+            let user = users.find((user) => post.dataValues.userId === user.id);
+            let userName = user.dataValues.name;
+
             elem = {
                 id : post.dataValues.id,
                 song: post.dataValues.song,
@@ -25,6 +31,8 @@ router.get('/songs', async (req, res) => {
                 postDescription: post.dataValues.postDescription,
                 catUrl: catUrl,
                 catName : catName,
+                updatedAt: post.dataValues.updatedAt,
+                creator : userName,
             }
 
             newPosts.push(elem);
