54d0d4b7b2e1d4cabcc5a69d6732c36ce0404325
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-01-21 18:25:33 +0100

    updated favourites and likes with transactions

--- routers/favourites.js
+++ routers/favourites.js
@@ -1,6 +1,7 @@
 const Router = require("express");
 const router = new Router();
 const authMiddleware = require("../auth/middleware");
+const {sequelize} = require("../models");
 const Favourites = require("../models").favorite;
 
 router.delete('/favourite/:userId/:postId', authMiddleware, async (req, res) => {
@@ -8,39 +9,54 @@ router.delete('/favourite/:userId/:postId', authMiddleware, async (req, res) => 
         const postId = parseInt(req.params.postId);
         const userId = parseInt(req.params.userId);
 
-        const favouriteToDelete = await Favourites.findOne({where : {postId : postId, userId : userId}});
+        const favouriteToDelete = await Favourites.destroy({
+            where: {
+                postId: postId,
+                userId: userId
+            }
+        });
 
-        if (!favouriteToDelete) {
-            res.status(404).send(`Favourite for post number ${postId} by user number ${userId} not found`);
-        } else {
-            const deleted = await favouriteToDelete.destroy();
-            res.json(deleted);
-        }
-
-        console.log("deleted", deleted);
-
+        console.log("deleted", favouriteToDelete);
+        res.json(favouriteToDelete);
     } catch (e) {
         console.log(e.message);
     }
 });
 
-router.post("/favourite", authMiddleware, async (req, res) => {
+router.put("/favourite", authMiddleware, async (req, res) => {
+    let transaction;
+    let newFavourite;
     try {
         const postId = req.body.postId;
         const userId = req.body.userId;
 
-        const newFavourite = await Favourites.create({
-            userId : userId,
-            postId : postId,
+        transaction = await sequelize.transaction();
+
+        const favouriteExist = await Favourites.findOne({
+            where: {
+                postId: postId,
+                userId: userId
+            }
         });
 
-        console.log("newFavourite", newFavourite);
+        if (favouriteExist) {
+            console.log("favourite already exist", favouriteExist);
+            newFavourite = favouriteExist;
+        } else {
+            newFavourite = await Favourites.create({
+                userId: userId,
+                postId: postId,
+            });
+        }
 
-        res.send(newFavourite);
+        await transaction.commit();
 
     } catch (e) {
+        if (transaction) await transaction.rollback();
         console.log(e.message);
     }
+
+    res.send(newFavourite);
 });
 
 module.exports = router;
--- routers/likes.js
+++ routers/likes.js
@@ -1,6 +1,7 @@
 const Router = require("express");
 const router = new Router();
 const authMiddleware = require("../auth/middleware");
+const {sequelize} = require("../models");
 const Likes = require("../models").like;
 
 router.delete('/like/:userId/:postId', authMiddleware, async (req, res) => {
@@ -8,39 +9,54 @@ router.delete('/like/:userId/:postId', authMiddleware, async (req, res) => {
         const postId = parseInt(req.params.postId);
         const userId = parseInt(req.params.userId);
 
-        const likeToDelete = await Likes.findOne({where : {postId : postId, userId : userId}});
+        const likeToDelete = await Likes.destroy({
+            where: {
+                postId: postId,
+                userId: userId
+            }
+        });
 
-        if (!likeToDelete) {
-            res.status(404).send(`Like for post number ${postId} by user number ${userId} not found`);
-        } else {
-            const deleted = await likeToDelete.destroy();
-            res.json(deleted);
-        }
-
-        console.log("deleted", deleted);
-
+        console.log("deleted", likeToDelete);
+        res.json(likeToDelete);
     } catch (e) {
         console.log(e.message);
     }
 });
 
-router.post("/like", authMiddleware, async (req, res) => {
+router.put("/like", authMiddleware, async (req, res) => {
+    let transaction;
+    let newLike;
     try {
         const postId = req.body.postId;
         const userId = req.body.userId;
 
-        const newLike = await Likes.create({
-            userId : userId,
-            postId : postId,
+        transaction = await sequelize.transaction();
+
+        const likeExist = await Likes.findOne({
+            where: {
+                postId: postId,
+                userId: userId
+            }
         });
 
-        console.log("newLike", newLike);
+        if (likeExist) {
+            console.log("like already exist", likeExist)
+            newLike = likeExist;
+        } else {
+            newLike = await Likes.create({
+                userId: userId,
+                postId: postId,
+            });
+            console.log("newLike", newLike);
+        }
 
-        res.send(newLike);
+        await transaction.commit();
 
     } catch (e) {
+        if (transaction) await transaction.rollback();
         console.log(e.message);
     }
+    res.send(newLike);
 });
 
 module.exports = router;
